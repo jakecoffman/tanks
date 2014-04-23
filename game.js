be36b489cdc35bce652c26a77fcaf7abe5f88d68
jake <jake@coffshire.com>
2014-04-23 18:23:20 -0500

    Better to start with spaceship example.

--- game.js
+++ game.js
@@ -1,53 +1,52 @@
-var GameState = function(game) {
+// This example uses the Phaser 2.0.4 framework
+
+// Copyright © 2014 John Watson
+// Licensed under the terms of the MIT License
+
+var GameState = function (game) {
 };
 
 // Load images and sounds
-GameState.prototype.preload = function() {
-    this.game.load.image('ground', 'assets/gfx/ground.png');
-    this.game.load.image('player', 'assets/gfx/player.png');
+GameState.prototype.preload = function () {
+    this.game.load.spritesheet('ship', 'assets/gfx/ship.png', 32, 32);
 };
 
 // Setup the example
-GameState.prototype.create = function() {
-    // Set stage background to something sky colored
-    this.game.stage.backgroundColor = 0x4488cc;
+GameState.prototype.create = function () {
+    // Set stage background color
+    this.game.stage.backgroundColor = 0x333333;
 
-    // Define movement constants
+    // Define motion constants
+    this.ROTATION_SPEED = 180; // degrees/second
+    this.ACCELERATION = 200; // pixels/second/second
     this.MAX_SPEED = 250; // pixels/second
-    this.ACCELERATION = 600; // pixels/second/second
+    this.DRAG = 500; // pixels/second
 
-    // Create a player sprite
-    this.player = this.game.add.sprite(this.game.width/2, this.game.height - 64, 'player');
+    // Add the ship to the stage
+    this.ship = this.game.add.sprite(this.game.width / 2, this.game.height / 2, 'ship');
+    this.ship.anchor.setTo(0.5, 0.5);
+    this.ship.angle = -90; // Point the ship up
+//    this.ship.body.collideWorldBounds = true;
 
-    // Enable physics on the player
-    this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+    // Enable physics on the ship
+    this.game.physics.enable(this.ship, Phaser.Physics.ARCADE);
 
-    // Make player collide with world boundaries so he doesn't leave the stage
-    this.player.body.collideWorldBounds = true;
+    // Set maximum velocity
+    this.ship.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
 
-    // Set player maximum movement speed
-    this.player.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
+    // Add drag to the ship that slows it down when it is not accelerating
+    this.ship.body.drag.setTo(this.DRAG, this.DRAG); // x, y
 
     // Capture certain keys to prevent their default actions in the browser.
     // This is only necessary because this is an HTML5 game. Games on other
     // platforms may not need code like this.
+    //noinspection JSValidateTypes
     this.game.input.keyboard.addKeyCapture([
         Phaser.Keyboard.LEFT,
         Phaser.Keyboard.RIGHT,
         Phaser.Keyboard.UP,
         Phaser.Keyboard.DOWN
     ]);
-
-    // Create some ground for the player to walk on
-    this.ground = this.game.add.group();
-    for(var x = 0; x < this.game.width; x += 32) {
-        // Add the ground blocks, enable physics on each, make them immovable
-        var groundBlock = this.game.add.sprite(x, this.game.height - 32, 'ground');
-        this.game.physics.enable(groundBlock, Phaser.Physics.ARCADE);
-        groundBlock.body.immovable = true;
-        groundBlock.body.allowGravity = false;
-        this.ground.add(groundBlock);
-    }
 
     // Show FPS
     this.game.time.advancedTiming = true;
@@ -57,24 +56,45 @@ GameState.prototype.create = function() {
 };
 
 // The update() method is called every frame
-GameState.prototype.update = function() {
+GameState.prototype.update = function () {
     if (this.game.time.fps !== 0) {
         this.fpsText.setText(this.game.time.fps + ' FPS');
     }
 
-    // Collide the player with the ground
-    this.game.physics.arcade.collide(this.player, this.ground);
+    // Keep the ship on the screen
+    if (this.ship.x > this.game.width) this.ship.x = 0;
+    if (this.ship.x < 0) this.ship.x = this.game.width;
+    if (this.ship.y > this.game.height) this.ship.y = 0;
+    if (this.ship.y < 0) this.ship.y = this.game.height;
 
-    if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        // If the LEFT key is down, set the player velocity to move left
-        this.player.body.acceleration.x = -this.ACCELERATION;
-    } else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-        // If the RIGHT key is down, set the player velocity to move right
-        this.player.body.acceleration.x = this.ACCELERATION;
+    if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT) || this.input.keyboard.isDown(Phaser.Keyboard.A)) {
+        // If the LEFT key is down, rotate left
+        this.ship.body.angularVelocity = -this.ROTATION_SPEED;
+    } else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT) || this.input.keyboard.isDown(Phaser.Keyboard.D)) {
+        // If the RIGHT key is down, rotate right
+        this.ship.body.angularVelocity = this.ROTATION_SPEED;
     } else {
-        // Stop the player from moving horizontally
-        this.player.body.acceleration.x = 0;
-        this.player.body.velocity.x = 0;
+        // Stop rotating
+        this.ship.body.angularVelocity = 0;
+    }
+
+    if (this.input.keyboard.isDown(Phaser.Keyboard.UP) || this.input.keyboard.isDown(Phaser.Keyboard.W)) {
+        // If the UP key is down, thrust
+        // Calculate acceleration vector based on this.angle and this.ACCELERATION
+        this.ship.body.acceleration.x = Math.cos(this.ship.rotation) * this.ACCELERATION;
+        this.ship.body.acceleration.y = Math.sin(this.ship.rotation) * this.ACCELERATION;
+
+        // Show the frame from the spritesheet with the engine on
+        this.ship.frame = 1;
+    } else if (this.input.keyboard.isDown(Phaser.Keyboard.DOWN) || this.input.keyboard.isDown(Phaser.Keyboard.S)) {
+        this.ship.body.acceleration.x = -Math.cos(this.ship.rotation) * this.ACCELERATION;
+        this.ship.body.acceleration.y = -Math.sin(this.ship.rotation) * this.ACCELERATION;
+    } else {
+        // Otherwise, stop thrusting
+        this.ship.body.acceleration.setTo(0, 0);
+
+        // Show the frame from the spritesheet with the engine off
+        this.ship.frame = 0;
     }
 };
 
